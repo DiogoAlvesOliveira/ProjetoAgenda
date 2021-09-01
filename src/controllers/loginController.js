6709222a46e97e756c338a5b5ac07f696c036220
diogo.a.a.oliveira <diogo.a.a.oliveira@accenture.com>
2021-09-01 18:30:28 -0300

    Login 100%

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require('../models/LoginModel');
 
-exports.index = (re,res)=>{
-    res.render('login');
+exports.index = (req,res)=>{
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
+    
 };
 exports.register = async function (req,res) {
     try{
@@ -25,3 +27,31 @@ exports.register = async function (req,res) {
     }
     
 };
+exports.login = async function (req,res) {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(function (){
+            return res.redirect('../login/index');
+        });
+        return;
+        }
+        
+        req.flash('success', 'Você está logado');
+        req.session.user = login.user;
+        req.session.save(function (){
+        return res.redirect('../login/index');
+        });
+    } catch(e){
+        console.log('control', e);
+        res.render('404');
+    }
+    
+};
+exports.logout = async function (req, res) {
+    req.session.destroy();
+    res.redirect('/'); 
+}
